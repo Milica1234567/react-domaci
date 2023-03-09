93fd55a97ef4ac4e3bff07899f8c61978c5ae4f3
Milica1234567 <75258306+Milica1234567@users.noreply.github.com>
2023-03-09 13:49:58 +0100

    header

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
 import './home.css'
+import video from '../../assets/vid1.mp4'
+import {CiLocationOn} from 'react-icons/ci'
+import {BsCalendarDate} from 'react-icons/bs'
+import {BsFacebook} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+import {AiOutlinePhone} from 'react-icons/ai'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className="overplay"></div>
+      <video src={video} muted autoPlay loop type="video/mp4"></video>
+
+      <div className="homeContent container">
+        <div className="textDiv">
+
+          <span className="smallText">
+            Neki tekst
+          </span>
+
+          <h1 className='homeTitle'>
+            Zakažite svoj termin
+          </h1>
+        </div>
+
+        <div className="cardDiv grid">
+          <div className="lokacijaInput">
+            <label htmlFor="adresa">Vaša adresa:</label>
+            <div className="input flex">
+              <CiLocationOn className="icon"/>
+              <input type="text" placeholder='Unesite Vašu adresu ovde'/>
+            </div>
+          </div>
+
+
+          <div className="dateInput">
+            <label htmlFor="date">Odaberite datum:</label>
+            <div className="input flex">
+              <BsCalendarDate className="icon"/>
+              <input type="date" placeholder='Unesite Vašu adresu ovde'/>
+            </div>
+          </div>
+
+
+          <div className="zakazi flex">
+            
+            <button className='btn'>ZAKAŽI</button>
+          </div>
+
+          
+        
+        </div>
+
+        <div className="homeFooterIcons flex">
+          <div className="rightIcons">
+            <BsFacebook className="icon"/>
+            <BsInstagram className="icon"/>
+            <AiOutlinePhone className="icon"/>
+          </div>
+
+          <div className="leftIcons">
+            
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './main.css'
+import img1 from '../../assets/img1.jpg'
+
+
+const Data = [
+  {
+    id: 1,
+    imgSrc: img1,
+    artTitle: 'Lorem ipsum',
+    subtitle: 'Podnaslov',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non augue turpis. Phasellus id dictum neque, a vulputate purus. Donec vitae auctor nibh. Aliquam at sem vel felis commodo efficitur. Nullam nisi lectus, dapibus vitae mollis in, lacinia et enim. Donec quis semper dolor, vel finibus odio.',
+    readMore: '#'
+    },
+  
+]
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <section className='main container section'>
+
+      <div className="secTitle">
+        <h3 className="title">
+          Nove objave
+        </h3>
+      </div>
+      <div className="secContent grid">
+        {
+          Data.map(({id, imgSrc, artTitle, subtitle, description, readMore})=>{
+              return(
+                <div key={id} className="singleArtical">
+                  <div className="imageDiv">
+                    <img src={imgSrc} alt={artTitle}/>
+                  </div>
+
+                  <div className="cardInfo">
+                    <h4 className="artTitle">{artTitle}</h4>
+                    <span className="content felx">
+                      <h5>{subtitle}</h5>
+                      <p>{description}</p>
+                      <button className='btn'><a href={readMore}>Pročitaj više</a></button>
+                    </span>
+                  </div>
+                </div>
+              )
+          })
+        }
+      </div>
+
+    </section>
   )
 }
 
