9015d8a4439e7b3f853585d3fd2e25b2dc2ce9ce
Milica1234567 <75258306+Milica1234567@users.noreply.github.com>
2023-04-15 01:38:39 +0200

    about page

--- src/Components/About/Ab.jsx
+++ src/Components/About/Ab.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './about.css'
+import profilna from '../../assets/profilna.JPG'
 
 
 function Ab() {
   return (
-    <section className=''>
+    <section className='container-about'>
+      <div class="red-a">
+
+
+        <div class="o-meni-col">
+            <img class="profilna" src={profilna}></img>
+        </div>
+        <div class="o-meni-col">
+            <h1>Nešto ukratko o meni</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea velit distinctio porro pariatur aperiam
+                dolorum ducimus illo non aliquid.
+                Repellat ducimus ratione dolore cumque saepe vero expedita possimus sequi dicta.Lorem ipsum dolor
+                sit amet consectetur adipisicing elit. Ea velit distinctio porro pariatur aperiam dolorum ducimus
+                illo non aliquid.
+                Repellat ducimus ratione dolore cumque saepe vero expedita possimus sequi dicta.</p>
+            <p class="potpis">dr Jelena Todić</p>
+        </div>
+
+
+
+
+        </div>
 
     
 
