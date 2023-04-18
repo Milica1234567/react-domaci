52f8916dcd9bb8e761628096362a232a16aa80b7
Milica1234567 <75258306+Milica1234567@users.noreply.github.com>
2023-04-18 21:02:56 +0200

    fade @media za about i blog

--- src/Components/About/Ab.jsx
+++ src/Components/About/Ab.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './about.css'
 import profilna from '../../assets/profilna.JPG'
+import Aos, { init } from 'aos'
+import 'aos/dist/aos.css'
 
 
-function Ab() {
+const Ab = () => {
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  }, [])
+
   return (
     <section className='container-about'>
       <div class="red-a">
 
         <div class="o-meni-col">
-            <img class="profilna" src={profilna}></img>
+            <img data-aos="fade-right" class="profilna" src={profilna}></img>
         </div>
 
         <div class="o-meni-col">
@@ -20,7 +26,7 @@ function Ab() {
                 sit amet consectetur adipisicing elit. Ea velit distinctio porro pariatur aperiam dolorum ducimus
                 illo non aliquid.
                 Repellat ducimus ratione dolore cumque saepe vero expedita possimus sequi dicta.</p>
-            <p class="potpis">dr Jelena Todić</p>
+            <p data-aos="fade-left" class="potpis">dr Jelena Todić</p>
         </div>
       </div>
   </section>
